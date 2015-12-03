1e11a35967ee89fb9f32dddf9df7da11de697681
Renzo Carpio <renzedu@hotmail.com>
2015-12-03 12:35:34 -0500

    VS Project

---
+++ HelloWorldGitHubWebApplication/HelloWorldGitHubWebApplication/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HelloWorldGitHubWebApplication.MvcApplication" Language="C#" %>
---
+++ HelloWorldGitHubWebApplication/HelloWorldGitHubWebApplication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HelloWorldGitHubWebApplication.Startup))]
+namespace HelloWorldGitHubWebApplication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
